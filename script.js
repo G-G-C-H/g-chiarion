b61852c6248e95779e3fc9180749723716766766
GG-CH <giorgia@wonderbly.com>
2022-05-16 20:21:18 +0100

    new beginning for personal site

---
+++ script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+console.log( "ready!" );
+
+console.log("I'm here, I'm here!!")
+
+
+});
+
+$(document).ready(function() {
+    $(function() {
+    $("#open-sesame").click(function() {
+      $("#first-container").toggle();
+    });
+  });
+  });
